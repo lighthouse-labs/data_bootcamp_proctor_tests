fbc6ee3f7898fee4cf28ca3573ad6d629df1c8e2
juraj_kapasny <juraj@knoyd.com>
2020-08-13 18:36:23 +0200

    solutions new

--- exam-data-04/solutions/solution_04.py
+++ exam-data-04/solutions/solution_04.py
@@ -1,7 +1,9 @@
-SQL = """SELECT d.department_id, 
-                d.department_name, 
-                e.first_name 
-         FROM departments d 
-            INNER JOIN employees e 
-            ON d.department_id = e.department_id
-         ORDER BY first_name"""
+SQL = """
+         select d.department_name
+            ,count(*) as number_of_employees
+         from employees as e
+         inner join departments as d
+         on e.department_id = d.department_id
+         group by d.department_name
+         order by number_of_employees desc
+      """
--- exam-data-04/unit-tests/test_02.py
+++ exam-data-04/unit-tests/test_02.py
@@ -11,12 +11,12 @@ def test_on_data_type():
     assert isinstance(compute_total_sale(df), pd.DataFrame)
 
 def test_on_correct_shape():
-    assert compute_total_sale(df).shape == (8, 1)
+    assert compute_total_sale(df).shape == (8, 3)
 
 def test_on_ordered_values():
     res = compute_total_sale(df)
     assert res['Sale_amt'].equals(res.sort_values('Sale_amt', ascending=False)['Sale_amt'])
 
 def test_on_correct_answer():
-    assert compute_total_sale(df).equals(df.groupby(['Region','Manager'])[['Sale_amt']].sum().sort_values('Sale_amt',ascending=False))
+    assert compute_total_sale(df).equals(df.groupby(['Region','Manager'])[['Sale_amt']].sum().sort_values('Sale_amt', ascending=False).reset_index())
 
